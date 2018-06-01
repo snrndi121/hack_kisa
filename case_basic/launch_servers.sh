7aa00c0ee795bd678ac1c754321918e9817de603
Ubuntu <ubuntu@ip-172-31-35-122.ap-northeast-1.compute.internal>
2018-06-01 21:33:47 +0000

    2 peer with score setting

--- case_basic/launch_servers.sh
+++ case_basic/launch_servers.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 export TAG=RC1.19.2
-export SSH_KEY_FOLDER=/home/ubuntu/.ssh/
 
 # Make log folder.
 export LOG_FOLDER=$(pwd)/logs
@@ -36,23 +35,8 @@ mkdir -p storage0
 docker run -d --name peer0 \
 -v $(pwd)/conf:/conf \
 -v $(pwd)/storage0:/.storage \
--v ${SSH_KEY_FOLDER} \
--e "DEFAULT_SCORE_HOST=github.com" \
 --link radio_station:radio_station \
 --log-driver fluentd --log-opt fluentd-address=localhost:24224 --log-opt tag=docker.{{.Name}} \
 -p 7100:7100 -p 9000:9000 \
-loopchain/looppeer:${TAG} \
-python3 peer.py -o /conf/peer_conf.json  -r radio_station:7102
-
-# Peer 1
-mkdir -p storage1
-docker run -d --name peer1 \
--v $(pwd)/conf:/conf \
--v $(pwd)/storage1:/.storage \
--v ${SSH_KEY_FOLDER} \
--e "DEFAULT_SCORE_HOST=github.com" \
---link radio_station:radio_station \
---log-driver fluentd --log-opt fluentd-address=localhost:24224 --log-opt tag=docker.{{.Name}} \
--p 7101:7101 -p 9001:9001 \
 loopchain/looppeer:${TAG} \
 python3 peer.py -o /conf/peer_conf.json  -r radio_station:7102
--- case_with_user_score/launch_servers.sh
+++ case_with_user_score/launch_servers.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 export TAG=RC1.19.2
-
+export SSH_KEY_FOLDER=/home/ubuntu/.ssh/id_tutorial
 
 # Make log folder.
 export LOG_FOLDER=$(pwd)/logs
@@ -38,8 +38,27 @@ docker run -d --name peer0 \
 -v $(pwd)/conf:/conf \
 -v $(pwd)/storage0:/.storage \
 -v $(pwd)/score:/score \
+-v ${SSH_KEY_FOLDER} \
+-e "DEFAULT_SCORE_HOST=github.com" \
 --link radio_station:radio_station \
 --log-driver fluentd --log-opt fluentd-address=localhost:24224 --log-opt tag=docker.{{.Name}} \
 -p 7100:7100 -p 9000:9000 \
 loopchain/looppeer:${TAG} \
 python3 peer.py -o /conf/peer_conf.json  -r radio_station:7102
+
+
+# Peer 1
+mkdir -p storage1
+
+docker run -d --name peer1 \
+-v $(pwd)/conf:/conf \
+-v $(pwd)/storage1:/.storage \
+-v $(pwd)/score:/score \
+-v ${SSH_KEY_FOLDER} \
+-e "DEFAULT_SCORE_HOST=github.com" \
+--link radio_station:radio_station \
+--log-driver fluentd --log-opt fluentd-address=localhost:24224 --log-opt tag=docker.{{.Name}} \
+-p 7101:7101 -p 9001:9001 \
+loopchain/looppeer:${TAG} \
+python3 peer.py -o /conf/peer_conf.json  -r radio_station:7102
+
