822629fc07c982c7cba9df0ab2ddb42b47f35dfb
snrndi121 <snrndi4521@gmail.com>
2018-06-04 20:57:19 +0900

    mock up : web interface

--- loopchain/case_basic/list_peer.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-curl http://localhost:9002/api/v1/peer/list | python3 -m json.tool
--- loopchain/case_with_user_score/state_peer.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-curl http://localhost:9000/api/v1/status/peer | python3 -m json.tool
